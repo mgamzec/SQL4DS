96e4081c59c31df48edc72e56eaac0d19fb0f7ea
Merve Gamze Çınar <62151645+mgamzec@users.noreply.github.com>
2024-06-10 14:54:33 +0300

    Add files via upload

---
+++ Intermediate_SQL_for_Data_Scientists/CH03/03_01.sql
@@ -0,0 +1,122 @@
+﻿select
+  *
+from
+  data_sci.company_departments
+
+
+
+
+
+
+select
+  upper(department_name)
+from
+  data_sci.company_departments
+
+
+
+
+select
+  initcap(department_name)
+from
+  data_sci.company_departments
+
+
+
+
+select
+  lower(initcap(department_name))
+from
+  data_sci.company_departments
+
+
+
+
+
+
+select
+  ‘ kelly’
+
+
+select
+  ‘ kelly’ = ‘kelly’
+
+
+
+
+select
+  ltrim(‘ kelly’)
+
+
+
+
+select
+  ltrim(‘ kelly’) = ‘kelly’
+
+
+
+
+select
+  ltrim(‘ kelly  ’) = ‘kelly’
+
+
+
+
+
+
+select
+  rtrim(ltrim(‘ kelly  ’)) = ‘kelly’
+
+
+
+
+select
+   job_title || last_name
+from
+  data_sci.employees
+
+
+select
+   job_title || ‘-’ || last_name
+from
+  data_sci.employees
+
+
+
+
+
+
+select
+   job_title || ‘-’ || null
+from
+  data_sci.employees
+
+
+
+-- Eger bir null değer varsa || yerine CONCAT fonksiyonu kullanılmalı
+select
+   concat(job_title, ‘-’,last_name, email)
+from
+  data_sci.employees
+
+
+
+
+
+
+select
+   concat(job_title, ‘-’,null)
+from
+  data_sci.employees
+
+-- WS: width seperater. Uzun bir liste oldugunda kullanıslıdır
+select
+   concat_ws(‘-’, job_title, last_name)
+from
+  data_sci.employees
+
+
+select
+   concat_ws(‘-’, job_title, last_name,email)
+from
+  data_sci.employees
---
+++ Intermediate_SQL_for_Data_Scientists/CH03/03_02.sql
@@ -0,0 +1,56 @@
+﻿03_02
+
+
+select
+   ‘abcdefghijk’  test_string
+
+
+
+-- İlk 3 karakteri istiyoruz
+
+select
+   substring(‘abcdefghijk’, 1,3) test_string
+
+
+
+
+select
+   substring(‘abcdefghijk’ from 1 for 3) test_string
+
+
+
+-- 5. dizeden baslayıp sonuna kadar alacak
+select
+   substring(‘abcdefghijk’ from 5) test_string
+
+
+select
+  *
+from
+  employees
+
+
+
+
+select
+  *
+from
+  employees
+where
+   job_title like ‘%assistant%’
+
+
+
+
+
+
+select
+   job_title, (job_title like %assistant%) is_assistant
+from
+  employees
+where
+   job_title like ‘%assistant%’
+select
+   job_title, (job_title like %assistant%) is_assistant
+from
+  employees
