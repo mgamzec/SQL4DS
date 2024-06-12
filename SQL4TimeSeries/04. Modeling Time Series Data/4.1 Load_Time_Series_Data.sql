d754c80cfa2779f19c1d04351f4e883e9fcc94ab
Merve Gamze Çınar <62151645+mgamzec@users.noreply.github.com>
2024-06-12 23:05:07 +0300

    Add files via upload

---
+++ SQL4TimeSeries/04. Modeling Time Series Data/4.1 Load_Time_Series_Data.sql
@@ -0,0 +1,9 @@
+﻿/* 
+
+Loading Data 
+
+*/
+
+
+COPY time_series.location_temp(event_time, location_id, temp_celcius)
+FROM 'C:\Users\merve\Desktop\SQL_Time_Series\04\data\location_temp.txt' DELIMITER ',';
